5b0b0387c7df2d5c77856cdd7bf5f2618e36eae2
alisinabh <alisina.bm@gmail.com>
2016-12-28 14:52:03 +0330

    CSharp Package added

---
+++ CSharp/ZgTimeTable/ZgTimeTable/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZgTimeTable")]
+[assembly: AssemblyDescription("Zigor protocol time table implementations")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Resaneh24")]
+[assembly: AssemblyProduct("ZgTimeTable")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("Resaneh24")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5b4d731f-2351-4bd7-b292-cff3c2f565d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
